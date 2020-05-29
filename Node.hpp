13e7da47027ebf120ba4037dc30d68efa95c740f
ms5919 <ms5919@imperial.ac.uk>
2020-05-29 15:09:59 +0100

    Prepared API to calculate node voltages.

--- Node.hpp
+++ Node.hpp
@@ -15,10 +15,30 @@ class Nodes
 {
 private:
     map<pair<string, string>, vector<Component>> Branch;
+    vector<vector<double>> Conductances;
+    vector<double> Currents;
+    map<string, int> Labels;
+    unsigned int Size;
 public:
+    Nodes()
+    {
+        Size = 0;
+    }
     void add_branch(const pair<string, string> &p, const Component &c)
     {
         Branch[p].push_back(c);
+        //dynamicly calculate Currents and Conductances
+    }
+}
+
+int node_number(string &s)
+{
+    if(s[0] == 'n' || s[0] == 'N')
+        //map to number after n
+    else
+    {
+        //check label
+        //if new, add new label with number Size+1
     }
 }
 
---
+++ calculate_voltages.cpp
@@ -0,0 +1,15 @@
+#include "Components.hpp"
+#include "Node.hpp"
+#include "Analysis.hpp"
+#include <iostream>
+#include <cstring>
+#include <sstream>
+#include <cmath>
+#include <utility>
+
+
+
+vector<double> node_voltages(vector<vector<double>> &conductances, vector<double> &node_currents)
+{
+    //compute node voltages;
+}
